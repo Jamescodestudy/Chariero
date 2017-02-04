33a41c6b8c942b46e9a6b8192a1d70b169e4b5f2
Jakub Budzynski <james.codestudy@gmail.com>
2017-02-04 23:57:33 +0100

    Added Second Chart

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,31 +3,70 @@ console.log(CHART);
 
 let lineChart = new Chart(CHART, {
      type: 'line',
-     data:    {
-          labels: ["January", "February", "March", "April", "May", "June", "July"],
-          datasets: [
-        {
-            label: "Miesięczne Wyniki",
-            fill: false,
-            lineTension: 0.3,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [100, 59, 80, 81, 0, 55, 40],
-            spanGaps: false,
-        }
-    ]
-    }
+     data:
+     {
+          labels: ["Matematyka", "Polski", "Angielski", "Fizyka", "Informatyka", "Chemia", "Biologia"],
+          datasets:
+          [
+               {
+                    label: "Wyniki Maturalne",
+                    fill: false,
+                    lineTension: 0.3,
+                    backgroundColor: "rgba(75,192,192,0.4)",
+                    borderColor: "rgba(75,192,192,1)",
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: "red",
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [50,40,30,50,78,43.5,23.8],
+                    spanGaps: false,
+               },
+               {
+                    label: "II LO",
+                    fill: true,
+                    lineTension: 0.3,
+                    backgroundColor: "rgba(75,192,192,0.4)",
+                    borderColor: "rgba(75,240,192,1)",
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: "red",
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: "rgba(75,240,192,1)",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [80,60,50,80,45.7,20,32.1],
+                    spanGaps: false,
+               }
+          ]
+     },
+     options:
+     {
+          scales:
+          {
+               yAxes:
+               [
+                    {
+                         ticks:
+                         {
+                              beginAtZero: true
+                         }
+                    }
+               ]
+          }
+     }
 });
